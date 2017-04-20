e80a5f7f9f0c99a0bbb3c19a4058acc5c4624b2e
selamatklik <krishna.aurelio@yahoo.com>
2017-04-20 21:36:42 +0700

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,16 @@
+/*
+ * Leave Report
+ * Write an expression that calculates the average number of leave days taken by the employees in this year. Replace your_answer with your expression.
+ */
+
+var annualLeave = 30
+var BobRemainingDays = 12
+var AliceRemainingDays = 28
+var TomRemainingDays = 19
+var average = ((30-12)+(30-28)+(30-19))/3
+
+exports.average = average
+exports.annualLeave = annualLeave
+exports.BobRemainingDays = BobRemainingDays
+exports.AliceRemainingDays = AliceRemainingDays
+exports.TomRemainingDays = TomRemainingDays
